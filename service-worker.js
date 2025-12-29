1f175fb358339fbb03c1a08b02afe3ca66c89c64
mohabathakimi848-ctrl <mohabathakimi848@gmail.com>
2025-12-28 22:13:40 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "retreat-makeready-v4";
+const CACHE = "retreat-makeready-v5"; // 🔥 NEW VERSION
 
 const ASSETS = [
   "./",
@@ -26,16 +26,6 @@ self.addEventListener("activate", (e) => {
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached;
-
-      return fetch(e.request).then(res => {
-        if (res && res.status === 200 && res.type === "basic") {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-        }
-        return res;
-      });
-    })
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
