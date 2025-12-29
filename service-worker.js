1c0f4fa83e84308322d052c1f7dc899d5300a53c
mohabathakimi848-ctrl <mohabathakimi848@gmail.com>
2025-12-28 21:57:55 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "retreat-makeready-v3";
+const CACHE = "retreat-makeready-v4";
 
 const ASSETS = [
   "./",
@@ -10,8 +10,9 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", (e) => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
   );
 });
 
@@ -19,12 +20,22 @@ self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request))
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(e.request).then(res => {
+        if (res && res.status === 200 && res.type === "basic") {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      });
+    })
   );
 });
